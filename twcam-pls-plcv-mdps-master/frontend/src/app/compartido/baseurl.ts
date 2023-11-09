95fdd3ec16d9aa973f32475f758c676d3c2305d4
ssarabia <ssarabia@godaddy.com>
2023-11-09 15:40:52 +0100

    add git ignore

--- twcam-pls-plcv-mdps-master/frontend/src/app/compartido/baseurl.ts
+++ twcam-pls-plcv-mdps-master/frontend/src/app/compartido/baseurl.ts
@@ -1,2 +1,2 @@
 export const baseURL = 'http://localhost:3000/';
-export const baseAPIURL = 'http://localhost:8080/plc-pls-mps-tutorial/api/'; // <1>
+export const baseAPIURL = 'http://localhost:8080/proyecto-discoteca/api/'; // <1>
