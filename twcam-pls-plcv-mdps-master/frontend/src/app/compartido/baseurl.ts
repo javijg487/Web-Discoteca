55978eb45a37052ba8e887cb59d58ca484b02f4e
Javier Jiménez <javijg487@gmail.com>
2024-01-11 20:30:38 +0100

    Cambio de baseApiURL

--- twcam-pls-plcv-mdps-master/frontend/src/app/compartido/baseurl.ts
+++ twcam-pls-plcv-mdps-master/frontend/src/app/compartido/baseurl.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export const baseURL = "http://localhost:3000/";
-export const baseAPIURL = "http://localhost:8080/proyecto-discoteca/api/"; // <1>
+export const baseAPIURL = "http://127.0.0.1:8080/proyecto-discoteca/api/"; // <1>
 
 export const httpOptions = {
   headers: new HttpHeaders({
